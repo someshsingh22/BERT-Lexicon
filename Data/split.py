e194c500bc1744436d2cf4d94ce0b5b1e18ca049
someshsingh22 <someshsingh9414@gmail.com>
2020-12-25 17:36:33 +0530

    Splitter

--- Data/split.py
+++ Data/split.py
@@ -1,15 +1,27 @@
+import os
+import argparse
 import pandas as pd
 from pathlib import Path
 from sklearn.model_selection import train_test_split
 
-PATH = Path(__file__).parents[1] / "Data"
+parser = argparse.ArgumentParser(description="Splitter")
+parser.add_argument("--destination", "-d", type=str, required=True)
+parser.add_argument("--csv", "-c", type=str, required=True)
+parser.add_argument("--split", "-s", type=str, default=0.7)
+args = parser.parse_args()
 
-df = pd.read_csv(PATH / "blp-items.csv")
+
+DATA_PATH = Path(__file__).parents[1] / "Data"
+CREATE_PATH = DATA_PATH / args.destination
+if not os.path.exists(CREATE_PATH):
+    os.mkdir(CREATE_PATH)
+
+df = pd.read_csv(DATA_PATH / args.csv)
 df = df[["spelling", "lexicality"]]
 
-train, dev = train_test_split(df, test_size=0.3, random_state=22)
+train, dev = train_test_split(df, test_size=1 - args.split, random_state=22)
 dev, test = train_test_split(dev, test_size=0.33, random_state=22)
 
-test.to_csv(PATH / "test.csv", index=False)
-train.to_csv(PATH / "train.csv", index=False)
-dev.to_csv(PATH / "dev.csv", index=False)
+test.to_csv(CREATE_PATH / "test.csv", index=False)
+train.to_csv(CREATE_PATH / "train.csv", index=False)
+dev.to_csv(CREATE_PATH / "dev.csv", index=False)
